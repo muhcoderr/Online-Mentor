77b3bc03f8197234bfaa5b358b06ae407ded221d
Muhammad Tariq <muhammad4948@gmail.com>
2023-08-14 10:49:04 +0500

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask , render_template
 import json
+from flask_sqlalchemy import SQLAlchemy
   
 # Open Config.json file with read mode
 with open('templates\config.json','r') as c:
@@ -17,6 +18,25 @@ if(local_server):
 else:
     # configure the SQL database, relative to the app instance folder
     app.config["SQLALCHEMY_DATABASE_URI"] = params['prod_uri']
+
+# create the extension
+db = SQLAlchemy()
+
+# initialize the app with the extension
+db.init_app(app)
+
+class Trainers(db.Model):
+    # id, name, department, introduction, picture, facebook, twitter, linkedin, instagram, date
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=True)
+    department = db.Column(db.String, nullable=True)
+    introduction = db.Column(db.String, nullable=True)
+    picture = db.Column(db.String, nullable=True)
+    facebook = db.Column(db.String, nullable=True)
+    twitter = db.Column(db.String, nullable=True)
+    linkedin = db.Column(db.String, nullable=True)
+    instagram = db.Column(db.String, nullable=True)
+    date = db.Column(db.String, nullable=True)
 
 # Pages Slug and functions
 
@@ -40,10 +60,16 @@ def courses_detais():
 
     return render_template('course-details.html', params=params)
 
-@app.route('/trainers') #decorator drfines the   
+@app.route('/trainers', methods = ['GET']) #decorator drfines the   
 def trainers():  
-
-    return render_template('trainers.html', params=params)
+    try:
+        trainer = Trainers.query.all()
+        print(trainer)
+        return render_template('trainers.html', params=params, trainer=trainer)
+    except Exception as e:
+        # Print the error for debugging purposes
+        print(f"Error fetching resume data: {e}")
+        return render_template('trainers.html', params=params, trainer = [])
 
 @app.route('/events') #decorator drfines the   
 def events():  
@@ -59,6 +85,11 @@ def pricing():
 def contact():  
 
     return render_template('contact.html', params=params)
+
+@app.route('/login') #decorator drfines the   
+def login():  
+
+    return render_template('login.html', params=params) 
 
 if __name__ =='__main__':  
     app.run(debug = True)  
